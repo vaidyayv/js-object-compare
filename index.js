cc275afae86091cb33df9e8d786fe3f6438a85d2
Yogesh Vaidya <yvaidya@asu.edu>
2022-03-31 13:23:16 -0700

    added initial code for the package

---
+++ index.js
@@ -0,0 +1,24 @@
+exports.objectCompare = function (a, b) {
+    if (typeof a !== typeof b) {
+        return false;
+    }
+    switch (typeof a) {
+        case "object":
+            const aKeys = Object.keys(a);
+            const aLength = aKeys.length;
+            const bKeys = Object.keys(b);
+            const bLength = bKeys.length;
+            if (aLength !== bLength) {
+                return false;
+            }
+            for (let i = 0; i < aLength; i++) {
+                const key = aKeys[i];
+                if (!objectCompare(a[key], b[key])) {
+                    return false
+                }
+            }
+            return true;
+        default:
+            return a === b;
+    }
+}
